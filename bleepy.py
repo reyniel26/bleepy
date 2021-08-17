7d27f58a525990b633f355053555f801f077d31a
Mariah Gift Miranda <rmescamillas26@gmail.com>
2021-08-17 20:32:52 +0800

    Update bleepy.py
    bleepy can now speech recognize

--- bleepy.py
+++ bleepy.py
@@ -32,8 +32,52 @@ class SpeechRecognition:
     def getSampleRate(self):
         return self.sample_rate
     
+    def isModelExist(self):
+        return os.path.exists(self.model)
+    
     def isModelNotExist(self):
-        if not os.path.exists(self.model):
-            print ("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+        if not self.isModelExist():
+            print ("Warning: Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
             exit (1)
+    
+    def isFileExist(self):
+        return os.path.exists(self.file)
+    
+    def isFileNotExist(self):
+        if not self.isFileExist():
+            print ("Warning: File: ("+self.getFile()+") not exist. Please input the directory of the file")
+            exit (1)
+    
+    def checkImportantFiles(self):
+        self.isModelNotExist()
+        self.isFileNotExist()
+    
+    def run(self, file):
+        self.setFile(file)
+
+        SetLogLevel(0)
+
+        self.checkImportantFiles()
+    
+        model = Model(self.getModel())
+        rec = KaldiRecognizer(model, self.getSampleRate())
+
+        process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i',
+                                    self.getFile(),
+                                    '-ar', str(self.getSampleRate()) , '-ac', '1', '-f', 's16le', '-'],
+                                    stdout=subprocess.PIPE)
+
+        #output time and words
+        rec.SetWords(True)
+
+        while True:
+            data = process.stdout.read(4000)
+            if len(data) == 0:
+                break
+            if rec.AcceptWaveform(data):
+                print(rec.Result())
+            else:
+                print(rec.PartialResult())
+
+        print(rec.FinalResult())
 
--- test-bleepy.py
+++ test-bleepy.py
@@ -1,39 +1,5 @@
-from vosk import Model, KaldiRecognizer, SetLogLevel
-import os
-import sys
-import wave
-import subprocess
+from bleepy import SpeechRecognition
 
-
-#ASK FILE NAME
+stt = SpeechRecognition()
 filename = input("What filename? ")
-
-SetLogLevel(0)
-
-if not os.path.exists("model"):
-    print ("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
-    exit (1)
-
-sample_rate=16000
-model = Model("model")
-rec = KaldiRecognizer(model, sample_rate)
-
-
-process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i',
-                            filename,
-                            '-ar', str(sample_rate) , '-ac', '1', '-f', 's16le', '-'],
-                            stdout=subprocess.PIPE)
-
-#output time and words
-rec.SetWords(True)
-
-while True:
-    data = process.stdout.read(4000)
-    if len(data) == 0:
-        break
-    if rec.AcceptWaveform(data):
-        print(rec.Result())
-    else:
-        print(rec.PartialResult())
-
-print(rec.FinalResult())
+stt.run(filename)
